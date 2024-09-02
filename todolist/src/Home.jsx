8c812f11ff69c385b9635888053d908a30a4b306
Itsrakshitha <rakshithasree2005@gmail.com>
2024-09-02 09:19:15 +0530

    bug fix

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import Create from './Create'
 import axios from 'axios'
-import { BsCircleFill,BsFillCheckCircleFill,BsFillTrashFill } from "react-icons/bs";
+import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from "react-icons/bs";
 
-function  Home() {
-    const[todos, setTodos] = useState([])
-    useEffect(() => {
-        axios.get('http://localhost:3001/get')
-        .then(result => setTodos(result.data))
-        .catch(err => console.log(err))
-    }, [])
+function Home() {
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3001/get')
+      .then(result => setTodos(result.data))
+      .catch(err => console.log(err))
+  }, [])
 
-    const handleEdit = () => {}
+  const handleEdit = () => { }
   return (
-  <div> 
-      <h2>Todo List </h2> 
+    <div className="home">
+      <h2>Todo List </h2>
       <Create />
       <br />
-    {
-         todos.length === 0
-        ?
-        <div><h2>No Record</h2></div>
-        :     
-        todos.map(todo => (
-          <div className = 'task'>
-            <div className='checkbox' onClick={handleEdit}>
-              <BsCircleFill className='icon'/>
-              <p>{todo.task}</p>
-          </div>
-          <div>
-            <span><BsFillTrashFill className='icon'/></span>
+      {
+        todos.length === 0
+          ?
+          <div><h2>No Record</h2></div>
+          :
+          todos.map(todo => (
+            <div className='task'>
+              <div className='checkbox' onClick={handleEdit}>
+                <BsCircleFill className='icon' />
+              </div>
+                <p>{todo.task}</p>
+              <div>
+                <span><BsFillTrashFill className='icon' /></span>
+              </div>
             </div>
-          </div>
           ))
-    }
+      }
     </div>
 
   )
